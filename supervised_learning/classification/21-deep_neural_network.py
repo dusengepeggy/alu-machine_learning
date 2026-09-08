2350d8b03f9ccca6839553e06b0bd6a8dbeb8b1e
dusengepeggy <dickinepeggy100@gmail.com>
2026-09-08 14:37:02 +0200

    fix: 21's question blank space errors

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -124,7 +124,7 @@ class DeepNeuralNetwork:
             alpha (float): learning rate
         """
         m = Y.shape[1]
-        
+
         for i in range(self.L, 0, -1):
 
             A_prev = cache["A" + str(i - 1)]
@@ -140,5 +140,4 @@ class DeepNeuralNetwork:
             da = np.matmul(W.T, dz)
             self.__weights['W' + str(i)] -= (alpha * dw)
             self.__weights['b' + str(i)] -= (alpha * db)
-            
-    
+                     
